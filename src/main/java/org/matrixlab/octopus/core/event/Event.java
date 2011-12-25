11df185aa671db804b4c0fde73fa695d8256cbbe
dave sinclair <stampy88@yahoo.com>
2011-12-25 18:02:29 -0500

    Added getAttributeAs

--- src/main/java/org/matrixlab/octopus/core/event/Event.java
+++ src/main/java/org/matrixlab/octopus/core/event/Event.java
@@ -1,6 +1,7 @@
 package org.matrixlab.octopus.core.event;
 
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
 
 import java.util.Map;
 
@@ -10,12 +11,51 @@ import java.util.Map;
 public class Event {
     private final Map<String, Object> data;
 
+    public Event(String attributeName, Object value) {
+        this.data = ImmutableMap.of(attributeName, value);
+    }
+
     public Event(Map<String, Object> data) {
         this.data = ImmutableMap.copyOf(data);
     }
 
+    public Event unionWith(Event event) {
+        Map<String, Object> newData = Maps.newHashMap(data);
+        newData.putAll(event.getData());
+
+        return new Event(newData);
+    }
+
     public Map<String, Object> getData() {
         return data;
+    }
+
+    public Integer getAttributeAsInteger(String attributeName) {
+        return ((Number) data.get(attributeName)).intValue();
+    }
+
+    public Short getAttributeAsShort(String attributeName) {
+        return ((Number) data.get(attributeName)).shortValue();
+    }
+
+    public Long getAttributeAsLong(String attributeName) {
+        return ((Number) data.get(attributeName)).longValue();
+    }
+
+    public Float getAttributeAsFloat(String attributeName) {
+        return ((Number) data.get(attributeName)).floatValue();
+    }
+
+    public Double getAttributeAsDouble(String attributeName) {
+        return ((Number) data.get(attributeName)).doubleValue();
+    }
+
+    public String getAttributeAsString(String attributeName) {
+        return (String) data.get(attributeName);
+    }
+
+    public Boolean getAttributeAsBoolean(String attributeName) {
+        return (Boolean) data.get(attributeName);
     }
 
     @Override
