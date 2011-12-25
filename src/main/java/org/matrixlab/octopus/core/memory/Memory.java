4b21719150a9f557f1ee96291673e80ac410e467
dave sinclair <stampy88@yahoo.com>
2011-12-25 17:29:58 -0500

    Initial version

---
+++ src/main/java/org/matrixlab/octopus/core/memory/Memory.java
@@ -0,0 +1,23 @@
+package org.matrixlab.octopus.core.memory;
+
+import org.matrixlab.octopus.core.event.Event;
+
+import java.util.Collection;
+
+/**
+ * A {@link Memory} is used to store {@link Event}s for a {@link org.matrixlab.octopus.core.processor.Processor}.
+ * <p/>
+ * Some {@link org.matrixlab.octopus.core.processor.Processor}s required memory to store computed or temporary values
+ * in order to compute a computation.  An example of this is the {@link org.matrixlab.octopus.core.processor.Sma} that
+ * contains a window length which is a number of {@link Event}s for which the average should be computed over.
+ *
+ * @author dave sinclair(david.sinclair@lisa-park.com)
+ */
+public interface Memory<T> {
+
+    void add(T value);
+
+    boolean remove(T value);
+
+    Collection<T> values();
+}
---
+++ src/main/java/org/matrixlab/octopus/core/memory/heap/HeapCircularBuffer.java
@@ -0,0 +1,47 @@
+package org.matrixlab.octopus.core.memory.heap;
+
+import com.google.common.collect.Lists;
+import org.matrixlab.octopus.core.memory.Memory;
+
+import java.util.Collection;
+
+/**
+ * @author dave sinclair(david.sinclair@lisa-park.com)
+ */
+class HeapCircularBuffer<T> implements Memory<T> {
+
+    private final T[] buffer;
+
+    private int currentIndex;
+
+    @SuppressWarnings("unchecked")
+    public HeapCircularBuffer(int n) {
+        buffer = (T[]) new Object[n];
+        currentIndex = 0;
+    }
+
+    @Override
+    public void add(T value) {
+        buffer[currentIndex] = value;
+
+        currentIndex = (currentIndex + 1) % buffer.length;
+    }
+
+    @Override
+    public boolean remove(T value) {
+        throw new UnsupportedOperationException("Remove not supported");
+    }
+
+    @Override
+    public Collection<T> values() {
+        Collection<T> values = Lists.newArrayListWithCapacity(buffer.length);
+
+        for (T item : buffer) {
+            if (item != null) {
+                values.add(item);
+            }
+        }
+
+        return values;
+    }
+}
