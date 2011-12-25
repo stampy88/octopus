d3cadf650de10e071fb985e556a524efee06bc81
dave sinclair <stampy88@yahoo.com>
2011-12-25 17:28:44 -0500

    Initial version

---
+++ src/main/java/org/matrixlab/octopus/util/Pair.java
@@ -0,0 +1,63 @@
+package org.matrixlab.octopus.util;
+
+import com.google.common.base.Objects;
+
+/**
+ * Data structure for holding a pair of objects.
+ *
+ * @author dave sinclair (dsinclair@chariotsolutions.com)
+ * @param <F> type of First object of Pair
+ * @param <S> type if Second object of Pair
+ */
+public class Pair<F, S> {
+
+    private final F first;
+    private final S second;
+
+    public Pair(F first, S second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public F getFirst() {
+        return first;
+    }
+
+    public S getSecond() {
+        return second;
+    }
+
+    @Override
+    public boolean equals(Object rhs) {
+        if (this == rhs) {
+            return true;
+        }
+        if (rhs == null || getClass() != rhs.getClass()) {
+            return false;
+        }
+
+        Pair otherPair = (Pair) rhs;
+
+        if (first != null && otherPair.first != null && (!first.equals(otherPair.first))) {
+            return false;
+        }
+
+        return second != null && otherPair.second != null && second.equals(otherPair.second);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(first, second);
+    }
+
+    @Override
+    public String toString() {
+        return Objects.toStringHelper(this).
+                add("first", first).
+                add("second", "second").toString();
+    }
+
+    public static <F, S> Pair<F, S> newInstance(F first, S second) {
+        return new Pair<F, S>(first, second);
+    }
+}
