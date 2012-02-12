5921ffeb46506750178542ae682670ab2dae4d79
dave sinclair <stampy88@yahoo.com>
2012-02-11 19:02:09 -0500

    Added integer constraint

--- src/main/java/org/lisapark/octopus/core/parameter/Constraints.java
+++ src/main/java/org/lisapark/octopus/core/parameter/Constraints.java
@@ -14,6 +14,51 @@ public class Constraints {
         return new ClassConstraint(message);
     }
 
+    public static Constraint<Integer> integerConstraintWithMinimumAndMessage(Integer minimum, String message) {
+        checkArgument(minimum != null, "minimum cannot be null");
+        checkArgument(message != null, "message cannot be null");
+
+        return new IntegerConstraint(message, minimum);
+    }
+
+    @Persistable
+    static class IntegerConstraint implements Constraint<Integer> {
+
+        private Integer min;
+        private Integer max;
+        private final String message;
+
+        private IntegerConstraint(String message, Integer min) {
+            this(message, min, Integer.MAX_VALUE);
+        }
+
+        private IntegerConstraint(String message, Integer min, Integer max) {
+            this.message = message;
+            this.min = min;
+            this.max = max;
+        }
+
+        @Override
+        public void validate(String name, Integer value) throws ConstraintException {
+            if (max == null && value < min) {
+                throw new ConstraintException(message);
+            }
+
+            if (value < min) {
+                throw new ConstraintException(message);
+            }
+
+            if (max != null && value > max) {
+                throw new ConstraintException(message);
+            }
+        }
+
+        @Override
+        public Constraint<Integer> copyOf() {
+            return new IntegerConstraint(message, min, max);
+        }
+    }
+
     @Persistable
     static class ClassConstraint implements Constraint<String> {
 
